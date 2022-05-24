d177e8370fc96fa39fc7394eac5d46b785c556c3
cavanosa <cavanosa@cavanosa.cav>
2022-05-24 21:54:00 +0200

    deprecated login

---
+++ appandroid/app/src/main/java/com/cavanosa/tutorialfirebase/HelloActivity.java
@@ -0,0 +1,31 @@
+package com.cavanosa.tutorialfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HelloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+        findViewById(R.id.sign_out_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    public void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(HelloActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
